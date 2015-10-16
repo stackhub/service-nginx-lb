ada45f9b432608ec4d618efc30c219c16291900f
Karthik Gaekwad <karthik.gaekwad@gmail.com>
2015-10-16 09:10:45 -0500

    use the leader ip for confd

--- 1.2/confd/confd.sh
+++ 1.2/confd/confd.sh
@@ -4,7 +4,7 @@
 # ahead of invoking the process.
 # 
 # Works around https://github.com/kelseyhightower/confd/issues/310
-sed s#SE_BACKEND_KEY#$SE_BACKEND_KEY# /etc/confd/conf.d/nginx.toml.template > /etc/confd/conf.d/nginx.toml
+sed s#STACKENGINE_BACKEND_KEY#$STACKENGINE_BACKEND_KEY# /etc/confd/conf.d/nginx.toml.template > /etc/confd/conf.d/nginx.toml
 
 # Okay now we can run confd 
-exec /usr/bin/confd -backend stackengine -node $STACKENGINE_HOSTIP:8443 -scheme https -auth-token $SE_API_TOKEN -interval 5
+exec /usr/bin/confd -backend stackengine -node $STACKENGINE_LEADER_IP:8443 -scheme https -auth-token $STACKENGINE_API_TOKEN -interval 5
